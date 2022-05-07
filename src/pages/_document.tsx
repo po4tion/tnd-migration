1ec346044c7394b94b36b88e49f7c40e518bae36
Donggyu Kim <po4tion0429@gmail.com>
2022-05-07 10:13:05 +0900

    chakra theme 설정, chakra/react react^18 호환성 경고 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
+import { ColorModeScript } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
+import { theme } from "../config";
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
