70fd3d971dbce90ac8c333efe1b6db60fa717c58
Donggyu Kim <po4tion0429@gmail.com>
2022-05-07 19:07:28 +0900

    메인 페이지 템플릿

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./layout";
+export * from "./template";
+export * from "./header";
+export * from "./transfer";
---
+++ src/components/template/index.ts
@@ -0,0 +1,2 @@
+export { default as TemplateASIS } from "./TemplateASIS";
+export { default as TemplateTOBE } from "./TemplateTOBE";
